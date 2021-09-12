27492397a842bb797d29490473992bc5b5508313
solareenlo <solareenlo@gmail.com>
2021-09-12 09:42:49 +0900

    add: 27-30

---
+++ 29_Converting-to-Strings.rs
@@ -0,0 +1,17 @@
+use std::fmt;
+
+struct Circle {
+    radius: i32
+}
+
+impl fmt::Display for Circle {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "Circle of redius {}", self.radius)
+    }
+}
+
+fn main() {
+    let circle = Circle { radius: 6 };
+    println!("{}", circle);
+    println!("{}", circle.to_string());
+}
